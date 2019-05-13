05d3271bcf6ec64f66a428cb994cff111e9fde19
hendr <hendr@DESKTOP-34S8R0K>
2019-05-13 16:46:19 +0200

    Reworked class OptionsViewModel

--- Set/OptionsViewModel.cs
+++ Set/OptionsViewModel.cs
@@ -8,13 +8,27 @@ namespace Set
 {
     class OptionsViewModel: ObservableObject, PageViewModel
     {
+        
         public OptionsViewModel(MainWindowViewModel mainwindow)
         {
             mwvm = mainwindow;
+            color.Add("red"); //Default colors for the cards of the playset
+            color.Add("green");
+            color.Add("blue");
         }
 
         #region Fields
         MainWindowViewModel mwvm;
+        List<string> color;
         #endregion
+        
+        public List<string> Color {
+
+        get=>color;
+        set=> color = value;
+
+
+            }
+
     }
 }
